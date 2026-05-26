ef121477748e51db125aca3cb433449d4c8fb34e
unknown <carvajal57685@gmail.com>
2026-05-25 22:14:32 -0500

    Corrección de archivo echo

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,12 +3,15 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
+// Detectamos dinámicamente si estamos en producción (SSL / HTTPS) o local
+const isProduction = window.location.protocol === 'https:';
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
+    wsPort: isProduction ? 443 : (import.meta.env.VITE_REVERB_PORT ?? 8080),
+    wssPort: isProduction ? 443 : (import.meta.env.VITE_REVERB_PORT ?? 8080),
+    forceTLS: isProduction,
     enabledTransports: ['ws', 'wss'],
 });
